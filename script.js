deab140e9a3a4ac44c92d6c6452e126235e9e3eb
marvin blunck <marvinblunck@gmail.com>
2023-05-31 23:22:17 -0700

    text stays displayed on screen when page is refreshed. Added clear button that is functioning

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//copied from mini project 5
 let timeDisplayEl = $('#currentDay');
 function displayTime() {
   let rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
@@ -6,20 +5,23 @@ function displayTime() {
 }
 displayTime();
 setInterval(displayTime, 1000);
-   
+//code to get any user input that was saved in localStorage and set
 $(".saveBtn").click(function (){
 let textBox=$(this).siblings(".description").val();
 let timeDisplay= $(this).parent().attr('id');
-localStorage.setItem(textBox, timeDisplay);
+localStorage.setItem(timeDisplay, textBox);
 })
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10")); 
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));  
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
     // TODO: Add code to display the current date in the header of the page.
+    $('#clearBtn').click(function(){
+      $(".description").val("");
+      localStorage.clear();
+    });
