b5c0652478e84bb7576c0baff2d290d3f7c85b35
marvin blunck <marvinblunck@gmail.com>
2023-05-31 22:29:32 -0700

    time displays and save button stores data into the local storage'

---
+++ script.js
@@ -0,0 +1,25 @@
+//copied from mini project 5
+let timeDisplayEl = $('#currentDay');
+function displayTime() {
+  let rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+displayTime();
+setInterval(displayTime, 1000);
+   
+$(".saveBtn").click(function (){
+let textBox=$(this).siblings(".description").val();
+let timeDisplay= $(this).parent().attr('id');
+localStorage.setItem(textBox, timeDisplay);
+})
+    // TODO: Add code to apply the past, present, or future class to each time
+    // block by comparing the id to the current hour. HINTS: How can the id
+    // attribute of each time-block be used to conditionally add or remove the
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+    //
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+    //
+    // TODO: Add code to display the current date in the header of the page.
